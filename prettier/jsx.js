9d636e496e941b28a08d83d35a6daf8432839775
FrancisTorp <jZcSCwQe297816@outlook.com>
2024-03-23 18:02:52 +0800

    id

---
+++ prettier/jsx.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+	extends: [
+		path.join(__dirname, '..', 'jsx.js'),
+		'aqua/prettier/jsx',
+		path.join(__dirname, 'prettier.js'),
+		'prettier/react',
+	],
+};
